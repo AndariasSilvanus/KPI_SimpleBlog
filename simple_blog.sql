0dbc5d72fa8d0c05ec0c1ad96b6e136913e5ad19
unknown <rita_miffies@yahoo.co.id>
2016-02-22 19:10:25 +0700

    File upload

--- simple_blog.sql
+++ simple_blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2016 at 03:28 AM
+-- Generation Time: Feb 22, 2016 at 01:09 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -53,19 +53,28 @@ CREATE TABLE IF NOT EXISTS `post` (
   `judul` varchar(100) NOT NULL,
   `konten` varchar(1000) NOT NULL,
   `tanggal` date NOT NULL,
+  `gambar` varchar(50) NOT NULL,
   `userid` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_postid` (`userid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
 
 --
 -- Dumping data for table `post`
 --
 
-INSERT INTO `post` (`id`, `judul`, `konten`, `tanggal`, `userid`) VALUES
-(2, 'The odd thing that happens when injustice benefits you', 'Frans de Waal, a professor of primate behaviour at Emory University, is the unlikely star of a viral video. His academic''s physique, grey jumper and glasses aren''t the usual stuff of a YouTube sensation. But de Waal''s research with monkeys, and its implications for human nature, caught the imagination of millions of people.\nIt began with a TED talk in which de Waal showed the results of one experiment that involved paying two monkeys unequally (see video, below). Capuchin monkeys that lived together were taken to neighbouring cages and trained to hand over small stones in return for food rewards. The researchers found that a typical monkey would happily hand over stone after stone when it was rewarded for each exchange with a slice of cucumber.', '2014-11-11', 1),
-(5, 'Mediterranean migrant crisis', 'Its Frontex border surveillance service will be strengthened and a military mandate sought to destroy people-smugglers'' boats. An emergency summit of EU leaders will be held on Thursday.\nAs the EU ministers met, fresh distress calls from migrant boats were received.\nThe crisis worsened at the weekend when hundreds of migrants were feared drowned as a boat capsized off Libya. The EU''s foreign policy chief, Federica Mogherini, said the 10-point package set out at talks in Luxembourg was a "strong reaction from the EU to the tragedies" and "shows a new sense of urgency and political will".\n"We are developing a truly European sense of solidarity in fighting human trafficking - finally so."', '2010-03-31', 1),
-(26, 'Percobaan Kedua', 'Smoga', '2014-10-14', 1);
+INSERT INTO `post` (`id`, `judul`, `konten`, `tanggal`, `gambar`, `userid`) VALUES
+(2, 'The odd thing that happens when injustice benefits you', 'Frans de Waal, a professor of primate behaviour at Emory University, is the unlikely star of a viral video. His academic''s physique, grey jumper and glasses aren''t the usual stuff of a YouTube sensation. But de Waal''s research with monkeys, and its implications for human nature, caught the imagination of millions of people.\nIt began with a TED talk in which de Waal showed the results of one experiment that involved paying two monkeys unequally (see video, below). Capuchin monkeys that lived together were taken to neighbouring cages and trained to hand over small stones in return for food rewards. The researchers found that a typical monkey would happily hand over stone after stone when it was rewarded for each exchange with a slice of cucumber.', '2014-11-11', '', 1),
+(5, 'Mediterranean migrant crisis', 'Its Frontex border surveillance service will be strengthened and a military mandate sought to destroy people-smugglers'' boats. An emergency summit of EU leaders will be held on Thursday.\nAs the EU ministers met, fresh distress calls from migrant boats were received.\nThe crisis worsened at the weekend when hundreds of migrants were feared drowned as a boat capsized off Libya. The EU''s foreign policy chief, Federica Mogherini, said the 10-point package set out at talks in Luxembourg was a "strong reaction from the EU to the tragedies" and "shows a new sense of urgency and political will".\n"We are developing a truly European sense of solidarity in fighting human trafficking - finally so."', '2010-03-31', '', 1),
+(26, 'Percobaan Kedua', 'Smoga', '2014-10-14', '', 1),
+(28, 'Post', 'konten', '2016-02-22', '', 1),
+(29, 'tambah', 'konteen', '2016-02-22', '', 1),
+(30, 'yo', 'ko', '2016-02-22', '', 1),
+(31, 'yo', 'ko', '2016-02-22', '', 1),
+(32, 'yo', 'ko', '2016-02-22', 'uploads/', 1),
+(33, 'yo', 'ko', '2016-02-22', 'uploads/', 1),
+(34, 'yo', 'ko', '2016-02-22', 'uploads/', 1),
+(35, 'yo', 'ko', '2016-02-22', 'uploads/Ken ga kimi twiticon saneaki2.jpg', 1);
 
 -- --------------------------------------------------------
 
